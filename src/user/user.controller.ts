7105878e701503fbe9dc98f4c49e23f082e80fa7
Mandy Nguyen <mandynguyen0611@gmail.com>
2025-06-26 16:58:32 -0400

    update controller (for login)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,17 @@
 /* eslint-disable prettier/prettier */
 import { Controller, Post, Body } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
+// import { CreateUserDto } from './dto/create-user.dto';
 import { LoginDto } from './dto/login.dto';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
- @Post('register')
-    register(@Body() createUserDto: CreateUserDto) {
-    return this.userService.register(createUserDto);
-}
+  //  @Post('register')
+  //     register(@Body() createUserDto: CreateUserDto) {
+  //     return this.userService.register(createUserDto);
+  // }
 
   @Post('login')
   login(@Body() loginDto: LoginDto) {
