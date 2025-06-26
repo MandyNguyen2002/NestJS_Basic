97d9ca69ad5f9d1129b98564a1c2e212b7c124bf
Mandy Nguyen <mandynguyen0611@gmail.com>
2025-06-26 16:58:20 -0400

    link to mongdodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 
+
 @Module({
-  imports: [UserModule],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI!), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
